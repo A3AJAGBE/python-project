c6b03a37ab1c2aff5a4a833e9aed7afe60bb75b3
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-04-04 20:31:25 +0100

    Create administrator login access request table

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ config = {
 link = mysql.connector.connect(**config)
 
 mycursor = link.cursor()
-mycursor.execute("CREATE TABLE Administrators (A_Id INT NOT NULL AUTO_INCREMENT, Name VARCHAR(255) NOT NULL, Username VARCHAR(255) NOT NULL, Password VARCHAR(50) NOT NULL, ConfirmPassword VARCHAR(255) NOT NULL , CONSTRAINT PK_Administrator PRIMARY KEY(A_Id))");
+mycursor.execute("CREATE TABLE LoginAccess (LoginAccess_Id INT NOT NULL AUTO_INCREMENT, Name VARCHAR(255) NOT NULL, Username VARCHAR(255) NOT NULL, Email VARCHAR(100) NOT NULL, CONSTRAINT PK_LoginAccess PRIMARY KEY(LoginAccess_Id))");
 
 link.close()
