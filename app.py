1127d19116879fd0fed5762c7402aa1923f39f0e
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-04-04 06:38:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
