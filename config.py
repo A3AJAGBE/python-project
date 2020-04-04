84a2d1c33af2f0b99f2b1c0e7904b6f8743cc50b
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-04-04 19:19:27 +0100

    Test connection to database

---
+++ config.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+config = {
+  'user': 'root',
+  'password': 'root',
+  'unix_socket': '/Applications/MAMP/tmp/mysql/mysql.sock',
+  'raise_on_warnings': True,
+}
+
+link = mysql.connector.connect(**config)
+
+mycursor = link.cursor()
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x)
+  
+link.close()
