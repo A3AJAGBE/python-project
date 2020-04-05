7a7fb66ad3f4fd9e534568a61b085b6be4734b34
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-04-05 03:13:07 +0100

    add import  mysql.connector

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, session
+import mysql.connector
 
 app = Flask(__name__)
+
+config = mysql.connector.connect(
+  user = 'root',
+  password ='root',
+  unix_socket = '/Applications/MAMP/tmp/mysql/mysql.sock',
+  raise_on_warnings = True,
+  database = 'DrugSystem'
+)
+myCursor = config.cursor()
 
 """ This is to display the home page of the website """
 @app.route('/')
@@ -23,9 +33,23 @@ def contact():
     return render_template('contact.html')
 
 """ This is to display the admin register page of the website """
-@app.route('/admin_register')
+@app.route('/admin_register', methods=["GET","POST"])
 def admin_register():
-    return render_template('admin_register.html')
+    success = ' '
+    if request.method == "POST":
+        name = request.form['name']
+        username = request.form['username']
+        password = request.form['password']
+        con_password = request.form['con_password']
+        myCursor.execute("INSERT INTO Administrators (Name, Username, Password, confirmPassword) VALUES (%s, %s, %s, %s)", (name, username, password, con_password) )
+        config.commit()
+        # myCursor.close()
+        success = 'Administrator successfully in the database'
+    return render_template('admin_register.html', success = success)
+
+myCursor.close()
+config.close()
+
 
 if __name__ == '__main__':
     app.run()
