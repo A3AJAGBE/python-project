a810750741b087514acf4946ea6280eda5eafef0
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-04-05 03:09:39 +0100

    add form validation script

--- static/script.js
+++ static/script.js
@@ -13,3 +13,53 @@ for (var i = 0; i < menu.length; i++) {
     this.className += " active";
   });
 }
+
+// validation for Administrator Registration
+$(document).ready(function() {
+  $("#reg").validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 4
+      },
+      username: {
+        required: true,
+        minlength: 7,
+        maxlength: 15
+      },
+      password: {
+        required: true,
+        minlength: 8
+      },
+      con_password: {
+        required: true,
+        minlength: 8,
+        equalTo: "[name='password']"
+      }
+
+    },
+    messages: {
+      name: {
+        required: 'Enter your name',
+        minlength: 'Name must be at least 4 characters'
+      },
+      username: {
+        required: 'Enter your username',
+        minlength: 'Username must be at least 7 characters',
+        maxlength: 'Username must not be above 15 characters'
+      },
+      password: {
+        required: 'Enter your password',
+        minlength: 'Password must be at least 8 characters'
+      },
+      con_password: {
+        required: 'Repeat your password',
+        minlength: 'Password must be at least 8 characters',
+        equalTo: 'Password not a match'
+      }
+    },
+    submitHandler: function(form) {
+      form.submit();
+    }
+  });
+});
