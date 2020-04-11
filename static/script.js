7dabad77ef26077d549865739bb1f22f63c4c4fb
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-04-11 03:07:39 +0100

    remove .active function

--- static/script.js
+++ static/script.js
@@ -80,19 +80,3 @@ $(document).ready(function() {
   });
 
 });
-
-//Script to change active menu according to the menu
-// Get aside
-var asideNav = document.getElementById("nav");
-
-// Get all <a class="nav"> inside aside
-var menu = asideNav.getElementsByClassName("nav");
-
-// Loop through the link and add the active class to the current/clicked link
-for (var i = 0; i < menu.length; i++) {
-  menu[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
